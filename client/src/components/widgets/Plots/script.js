00b05c1a9368fe49ece59221fe6f2749c6af386b
Brianna Major <brianna.major@kitware.com>
2023-12-27 18:23:28 -0500

    Remove unused code
    Signed-off-by: Brianna Major <brianna.major@kitware.com>

--- client/src/components/widgets/Plots/script.js
+++ client/src/components/widgets/Plots/script.js
@@ -129,22 +129,6 @@ export default {
         }
       },
     },
-    // syncSteps: {
-    //   immediate: true,
-    //   handler(newVal, oldVal) {
-    //     if (newVal === oldVal) {
-    //       return;
-    //     }
-    //     if (this.plotFetcher && this.plotFetcher.initialized) {
-    //       this.plotFetcher.setSyncAnimation(newVal);
-    //       this.plotFetcher.setCurrentTimestep(this.localTimeStep, true);
-    //     }
-    //     this.setInitialLoad(true);
-    //     this.plotType = PlotType.None;
-    //     this.fetchTimeStepData(this.currentTimeStep);
-    //     // this.displayCurrentTimeStep();
-    //   },
-    // },
   },
 
   methods: {
